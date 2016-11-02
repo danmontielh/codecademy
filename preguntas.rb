5b04f2199b72aaf9b6afb8c47a1754b65b2dab06
Dan Mon <Dan Mon>
2016-11-02 17:23:32 -0600

    completo

--- preguntas.rb
+++ preguntas.rb
@@ -3,3 +3,15 @@
 
 print "¿Cúal es tu nombre?"
 firt_name = gets.chomp.capitalize!
+
+print "¿Cúal es tu primer apellido"
+last_name = gets.chomp.capitalize!
+
+print "¿De qué ciudad eres?"
+city = gets.chomp.capitalize!
+
+print "¿De qué estado eres?"
+state = gets.chomp.upcase!
+
+
+p "Tu nombre es #{firt_name} y tu apellido es #{last_name}. Naciste en #{city}, #{state}"
