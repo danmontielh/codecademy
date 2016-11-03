93e0ce490a768d2d592031c684ff11e61ea9783f
Dan Mon <Dan Mon>
2016-11-02 21:00:06 -0600

    versiòn final

--- daffyduck.rb
+++ daffyduck.rb
@@ -1,1 +1,12 @@
-daffyduck.rb
+print "¿Qué es lo que quieres modificar? "
+user_input = gets.chomp
+user_input.downcase!
+
+if user_input.include? "mo"
+	user_input.gsub!(/mo/, "lo")
+
+else puts "No se encuentra"
+end
+
+
+p "Quedarias asi: #{user_input}"
